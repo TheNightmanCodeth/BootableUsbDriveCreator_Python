412decb375ca1d2906fdfc1118fde7b4515100f6
Plays2 <joyod3@gmail.com>
2014-12-09 10:58:34 -0500

    Formatting changes
    Made the welcome screen pretty and changed some colors around

--- USBMaker.py
+++ USBMaker.py
@@ -13,35 +13,35 @@ class colors:
     GREEN   = '\033[0;32m'
     RED     = '\033[0;31m'
     MAGENTA = '\033[0;95m'
+    LMAGENTA = '\033[1;95m'
     CYAN    = '\033[0;36m'
     WHITE   = '\033[1;37m'
     LCYAN   = '\033[1;36m'
     LGREEN  = '\033[1;32m'
     END     = '\033[0m'
 
-#Un-comment this if you want to see what the colors look like. I left it In just in case you want to test the colors like I did. 
 #Make sure when you're done using a color and you want to return to the default color, you add '+colors.END'
 #If you aren't returning to normal colors, adding 'colors.END' isn't necessary
 #Also make sure you use 'colors.END at the end of your script. If you don't the user's terminal will be stuck in the color you left it in.
-#==========================================#
-#                                          #
-#    print colors.BLUE +"BLUE"			   #
-#    print colors.GREEN +"GREEN"           #
-#    print colors.RED +"RED"               #
-#    print colors.MAGENTA +"MAGENTA"       #
-#    print colors.CYAN +"CYAN"             #
-#    print colors.WHITE +"WHITE"           #
-#    print colors.LCYAN +"LIGHT CYAN"      #
-#    print colors.LGREEN +"LIGHT GREEN"    #
-#                                          #
-#==========================================#
 
-#TODO: Make the main menu prettier
-#Here we start the actual program.
-print colors.GREEN +"Welcome to the fantastical bootable USB drive creator by " +colors.LGREEN +"Plays2" +colors.END;
-print colors.CYAN +'\n' +"This example is written in Pyton 2.7.6 and the source code is available at: " + url + " If you would like to learn some python(:"+ colors.END;
+#This is what the menu will look like. Use this for formatting
+##########################################################################################################
+#										                                                                 #
+#                    Welcome to the fantastical bootable USB drive creator by Plays2                     #
+#              This example is written in Pyton 2.7.6 and the source code is available at:               #
+#    https://github.com/plays2/BootableUsbDriveCreator_Python/If you would like to learn some python     #
+#										                                                                 #
+##########################################################################################################
 
-#Here we make sure the user is ready to go(:
+print colors.GREEN +"##########################################################################################################"	
+print "#										                         #"	
+print "#                    "+colors.WHITE +"Welcome to the fantastical bootable USB drive creator by " +colors.LGREEN +"Plays2"+colors.GREEN +"                     #"
+print colors.GREEN +"#              "+colors.WHITE +"This example is written in Pyton 2.7.6 and the source code is available at:"+colors.GREEN +"               #" 
+print "#"+colors.WHITE +"    "+url +  "If you would like to learn some python     "+colors.GREEN +"#"	
+print "#										                         #"	
+print "##########################################################################################################"
+
+#Here we make sure the user is ready to go
 go = raw_input('\n' +"When you're ready to get started, type 'go' and press enter: ");
 if go in ["go", "Go"]:
 	#first we set up some directories
@@ -71,8 +71,8 @@ if go in ["go", "Go"]:
 	IMGDIRIMG = IMGDIR +"Img.img"
 	#user is ready, now let's set up some information.
 	#Now we need the path of the iso to copy to USB. 
-	print colors.LCYAN +"Next, I need the path to your target ISO."
-	PATH = raw_input("Drag your target iso into the terminal window." +'\n' +"Tip: Make sure the path looks something like '/Users/*your username*/Path/To/iso' and not '~/Path/To/iso' " +'\n' +"Path:");
+	print colors.CYAN +"First, I need the path to your"+colors.MAGENTA +" target ISO." +colors.CYAN
+	PATH = raw_input("Drag your " +colors.MAGENTA +"target iso " +colors.CYAN +"into the terminal window." +'\n' +colors.WHITE +"Tip: Make sure the path looks something like '/Users/*your username*/Path/To/iso' and not '~/Path/To/iso' " +'\n' +colors.LCYAN +"Path:" );
 	#Now we have to convert the ISO image to an IMG. This is a mac thing and will have to be done differently on linux and windows (Eventually)
 	#########          #Arguments############# #img output dir#####   #Source iso dir
 	hdiutil = 'hdiutil convert -format UDRW -o %s %s' %(IMGDIRIMG, PATH) 
@@ -95,7 +95,7 @@ if go in ["go", "Go"]:
 	os.system('diskutil list')
 	ENTER2 = raw_input("Now plug your flash drive back in an press enter: ")
 	os.system('diskutil list')
-	USBID = raw_input("Now type the name of your flash drive on which you want to copy the iso" +'\n' +"Don't forget that the drive you chose will be formatted, so make sure you chose the right one!" +'\n' +"Tip: Your drive id looks something like 'disk2' and is located in the far-right column. Don't include the trailing numbers! ie: 's1' or 's2'" +'\n' +"USB ID: "")
+	USBID = raw_input("Now type the name of your flash drive on which you want to copy the iso" +'\n' +"Don't forget that the drive you chose will be formatted, so make sure you chose the right one!" +'\n' +"Tip: Your drive id looks something like 'disk2' and is located in the far-right column. Don't include the trailing numbers! ie: 's1' or 's2'")
 	#now we need to unmount the disk and transfer the img
 
 	#Make the USBID usable for the unmount command (add /dev/ to the beginning)
@@ -109,5 +109,5 @@ if go in ["go", "Go"]:
 	OUTUSBID = "/dev/r" +USBID
 	#We add the r in the hopes of reducing wait time
 	os.system('sudo dd if=%s of=%s bs=1m' %(OUTDIRIMG, OUTUSBID))
-		
+
 	exit();
